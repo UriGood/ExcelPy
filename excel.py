3e1e85ae799e751b1c2428faff6e475e85bb209a
urielmartinezflores <75589783+UriGood@users.noreply.github.com>
2022-06-07 16:40:41 -0500

    +se añaden correcciones de identacion y mensajes

--- excel.py
+++ excel.py
@@ -1,19 +1,10 @@
-# import pandas as pd
-# xls = pd.ExcelFile('-3 2DPEA.xls')
-# df=xls.parse('Sheet1')
-# print(df)
-
-# from xml.etree.ElementInclude import include
+from xml.etree.ElementInclude import include
 from xlrd import open_workbook
 from src.conexion_mysql import conexionDb
 import pandas as pd 
 import os
 
-# wb = open_workbook('-3 2DPEA.xls')
-# sheet = wb.sheet_by_index(0)
-# df = pd.DataFrame(sheet,index=range(48), columns=['Serial Number'])
-
-# print(df)
+status = True
 excelfiles = []
 contenido = os.listdir()
 for fichero in contenido:
@@ -28,7 +19,6 @@ for file in excelfiles:
         serialNumber = sheet.cell_value(49+x, 2)
         meterNo = sheet.cell_value(49+x, 1)
         if(serialNumber != ''):
-            print(serialNumber)
             panel_number = sheet.cell_value(2, 3)
             job_number = sheet.cell_value(3, 3)
             job_name = sheet.cell_value(4, 3)
@@ -38,23 +28,26 @@ for file in excelfiles:
             modbus_id = sheet.cell_value(32, 2)
 
             connection = conexionDb()
-
             # conexion con la base de datos 
             if connection.is_connected():
-                    print('conexión exitosa')
-                    # info_server = connection.get_server_info()
-                    # print(info_server)
-                    cursor = connection.cursor()
-                    sentencia = "INSERT INTO registros (panel_number,job_number,job_name,seal,type,modbus_id,serial_number,meter_no) VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}')".format(
-                        panel_number,
-                        job_number,
-                        job_name,
-                        seal,
-                        tipo,
-                        modbus_id,
-                        serialNumber,
-                        meterNo
-                        )
-                    cursor.execute(sentencia)
-                    connection.commit()
-                    print("registro insertado con exito")   
+                status = True
+                # info_server = connection.get_server_info()
+                # print(info_server)
+                cursor = connection.cursor()
+                sentencia = "INSERT INTO registros (panel_number,job_number,job_name,seal,type,modbus_id,serial_number,meter_no) VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}')".format(
+                    panel_number,
+                    job_number,
+                    job_name,
+                    seal,
+                    tipo,
+                    modbus_id,
+                    serialNumber,
+                    meterNo
+                    )
+                cursor.execute(sentencia)
+                connection.commit()
+        connection.close()
+    if(status == True):
+        print("registros del documento {0} insertado con exito".format(file))
+    else:
+        print("error al registrar")   
